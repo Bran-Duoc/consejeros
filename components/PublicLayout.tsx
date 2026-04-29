16649a6e1cad05387a01cc19b3570c594011a1ef
Bran Pizarro <bran.pizarro@duocuc.cl>
2026-04-28 22:00:31 -0400

    style: reduce vertical gap between hero and footer

--- components/PublicLayout.tsx
+++ components/PublicLayout.tsx
@@ -5,7 +5,7 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
   return (
     <div className="flex flex-col min-h-screen bg-transparent">
       <main className="flex-1 flex flex-col pt-0 sm:pt-0">
-        <div className="flex-1">
+        <div>
           {children}
         </div>
         <Footer />
