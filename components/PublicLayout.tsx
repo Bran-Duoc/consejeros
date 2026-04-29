ef03f2f6a43b6df294d61300978042952f031ccc
Bran Pizarro <bran.pizarro@duocuc.cl>
2026-04-28 21:48:12 -0400

    revert: vertical spacing optimizations on mobile

--- components/PublicLayout.tsx
+++ components/PublicLayout.tsx
@@ -4,8 +4,8 @@ import Footer from "./Footer";
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-[100dvh] overflow-hidden flex flex-col bg-transparent">
-      <main className="flex-1 overflow-y-auto custom-scrollbar pt-0 sm:pt-20 flex flex-col">
-        <div className="shrink-0">
+      <main className="flex-1 overflow-y-auto custom-scrollbar pt-4 sm:pt-20 flex flex-col">
+        <div className="flex-1 shrink-0">
           {children}
         </div>
         <Footer />
