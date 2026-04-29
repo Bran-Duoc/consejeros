021a769cd8870b1940ea73a19ac7380f5ba2ce8b
Bran Pizarro <bran.pizarro@duocuc.cl>
2026-04-28 21:49:28 -0400

    style: allow natural page scrolling and static footer behavior

--- components/PublicLayout.tsx
+++ components/PublicLayout.tsx
@@ -3,9 +3,9 @@ import Footer from "./Footer";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-[100dvh] overflow-hidden flex flex-col bg-transparent">
-      <main className="flex-1 overflow-y-auto custom-scrollbar pt-4 sm:pt-20 flex flex-col">
-        <div className="flex-1 shrink-0">
+    <div className="flex flex-col min-h-screen bg-transparent">
+      <main className="flex-1 flex flex-col pt-0 sm:pt-0">
+        <div className="flex-1">
           {children}
         </div>
         <Footer />
